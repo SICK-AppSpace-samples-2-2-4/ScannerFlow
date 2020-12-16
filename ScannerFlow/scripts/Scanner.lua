1c4896858a74fcce72ed0c2607dce52232ac8a7f
rdzanmi <mikolaj.rdzanek@sick.de>
2020-12-16 22:49:19 +0100

    Initial commit

---
+++ ScannerFlow/scripts/Scanner.lua
@@ -0,0 +1,16 @@
+--[[----------------------------------------------------------------------------
+
+  Application Name: ScannerFlow
+
+  Description:
+  Retrieving scans and viewing as point cloud, using the Flow
+  approach.
+
+  Required: Scanner device
+
+  This sample retrieves the scans from a remote device. The configuration of the OnNewScan
+  block has to match the device. Alternatively for testing purposes the scans can be retrieved
+  from files in resources by adapting the Flow using the according block.
+  The scans can be viewed in the PointCloud viewer on the webpage.
+
+------------------------------------------------------------------------------]]
---
+++ readme.md
@@ -0,0 +1,12 @@
+## ScannerFlow
+Retrieving scans and viewing as point cloud, using the Flow approach.  
+### Description
+This sample retrieves the scans from a remote device. The configuration of the OnNewScan
+block has to match the device. Alternatively for testing purposes the scans can be retrieved
+from files in resources by adapting the Flow using the according block.
+### How to Run
+This sample requires a connected remote Scanner device.
+The scans can be viewed in the PointCloud viewer on the webpage
+
+### Topics
+Remote-Device, Scan, Flow, Point-Cloud, Acquisition, Sample, SICK-AppSpace
